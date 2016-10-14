f68901681fa7f3131aa414086dd53aff3b7e5d90
sangmoonhwang <ghkdtkdans@gmail.com>
2016-10-13 23:09:40 -0400

    Add convolve testing script

---
+++ convolve.sh
@@ -0,0 +1,6 @@
+./convolve dog.png dog_convolve.png 1 5;
+./convolve dog.png dog_convolve.png 2 5;
+./convolve dog.png dog_convolve.png 4 5;
+./convolve dog.png dog_convolve.png 8 5;
+./convolve dog.png dog_convolve.png 16 5;
+./convolve dog.png dog_convolve.png 32 5;
