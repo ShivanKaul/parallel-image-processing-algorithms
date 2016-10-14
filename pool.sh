b2e191bda884cb72f246adc6e55b65f8eb801966
sangmoonhwang <ghkdtkdans@gmail.com>
2016-10-13 21:48:00 -0400

    Add testing script and change the picture to be compatible with pool

---
+++ pool.sh
@@ -0,0 +1,6 @@
+./pool dog.png dog_pool.png 1 5;
+./pool dog.png dog_pool.png 2 5;
+./pool dog.png dog_pool.png 4 5;
+./pool dog.png dog_pool.png 8 5;
+./pool dog.png dog_pool.png 16 5;
+./pool dog.png dog_pool.png 32 5;
---
+++ rectify.sh
@@ -0,0 +1,6 @@
+./rectify dog.png dog_rectify.png 1 5;
+./rectify dog.png dog_rectify.png 2 5;
+./rectify dog.png dog_rectify.png 4 5;
+./rectify dog.png dog_rectify.png 8 5;
+./rectify dog.png dog_rectify.png 16 5;
+./rectify dog.png dog_rectify.png 32 5;
